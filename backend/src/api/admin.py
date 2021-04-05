7c53aa95740d904498c58683947e2288ffbec440
Stark <udbhavsomani18@gmail.com>
2021-04-05 14:04:13 +0530

    Full project push

---
+++ backend/src/api/admin.py
@@ -0,0 +1,9 @@
+from .models import Article
+from django.contrib import admin
+
+# admin.site.register(Article)
+
+@admin.register(Article)
+class ArticleModel(admin.ModelAdmin):
+    list_filter = ('title', 'description')
+    list_display = ('title', 'description')
